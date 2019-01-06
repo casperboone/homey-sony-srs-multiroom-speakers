06dbdf0970379e72b23ea3238e8ba4fd930f9c29
Casper Boone <mail@casperboone.nl>
2019-01-06 17:09:27 +0100

    Add volume set/mute capabilities and update them periodically (#8)
    * Fix device class
    
    * Add volume set/mute capabilities and update them periodically

--- drivers/sony-srs-multiroom-speakers/device.js
+++ drivers/sony-srs-multiroom-speakers/device.js
@@ -9,8 +9,27 @@ class SonySrsMultiroomSpeaker extends Homey.Device {
         this.audio = new Audio(this.getData().scalarWebApiBaseUrl)
         this.avContent = new AvContent(this.getData().scalarWebApiBaseUrl)
 
+        setInterval(() => {
+            this.audio.getVolume().then(status => {
+                this.setCapabilityValue('volume_set', status.volume / 50)
+                this.setCapabilityValue('volume_mute', status.mute === "on")
+            })
+        }, 5000)
+
+        this.registerCapabilityListener('volume_set', volume => {
+            this.audio.setVolume(volume * 50)
+
+            return Promise.resolve(true)
+        })
+
+        this.registerCapabilityListener('volume_mute', mute => {
+            this.audio.setMute(mute)
+
+            return Promise.resolve(true)
+        })
+
         new Homey.FlowCardAction('volume_set').register().registerRunListener((args, state) => {
-            this.audio.setVolume(args.volume)
+            this.audio.setVolume(args.volume * 50)
 
             return Promise.resolve(true)
         })
